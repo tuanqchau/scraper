6e30fa5aa0620d24bb653eb48ca0a9d69c40d4c8
Tuan Chau <tuanqchau2810@gmail.com>
2024-04-16 11:54:23 -0400

    scraping title, price, location, and link

--- scraper.py
+++ scraper.py
@@ -1,8 +1,39 @@
+class Listing:
+    def __init__(self, title, location, size, price, link):
+        self.title = title
+        self.location = location
+        #self.size = size
+        self.price = price
+        self.link = link
+    
+    def __str__(self):
+        return f"{self.title} {self.location} {self.price} {self.link}"
+
 import requests
 from bs4 import BeautifulSoup
 
 url = 'https://washingtondc.craigslist.org/search/silver-spring-md/apa?lat=39.0553&laundry=1&lon=-77.0201&max_bedrooms=1&max_price=1550&search_distance=8.9#search=1~gallery~0~0'
 
-html = requests.get(url)
+response = requests.get(url)
 
-print(html.text)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+#print(soup.prettify())
+
+listings = soup.find_all('li', class_='cl-static-search-result')
+
+listing_objects = []
+
+for listing in listings:
+    title = listing.find('div', class_='title').text.strip()
+    price = listing.find('div', class_='price').text.strip()
+    location_tag = listing.find('div', class_='location')
+    location = location_tag.text.strip() if location_tag else "Location not specified"
+    link = listing.find('a')['href']
+    listing_objects.append(Listing(title, location, price, location, link))
+
+for listing in listing_objects:
+    print("Title:", listing.title)
+    print("Price:", listing.price)
+    print("Link:", listing.link)
+    print()
